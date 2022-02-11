83f7be14d006e26c6f1a6eefeebef0d67089aa7f
edy <edy@edydeMacBook-Pro.local>
2022-02-11 20:17:33 +0800

    初始化maven项目

---
+++ src/test/java/indi/com/base/BaseTest.java
@@ -0,0 +1,13 @@
+package indi.com.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BaseTest {
+
+    @Test
+    void baseTest(){
+        System.out.println("base test...");
+    }
+}
